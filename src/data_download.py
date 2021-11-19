e60f4b2c35cd5d53f2e1cbc50be2554f29909541
voremargot <margot.vore@outlook.com>
2021-11-19 15:24:46 -0800

    added opts variable

--- src/data_download.py
+++ src/data_download.py
@@ -1,19 +1,19 @@
 # author: Margot Vore
 # date: Nov 19, 2021
 
-""" This script reads in CSV from a given URL and saves it
-to your local machine.
-Usage: python data_download.py <url> <path>
+""" This script reads in CSV from a given URL and saves it to your local machine.
+Usage: python data_download.py <url> <path>    
 
 Options:
 <url>      Take the URL of the CSV you want to download
 <path>     Takes the path where you want the data to be saved to
-
 """
 
 
 from docopt import docopt
 import pandas as pd
+
+opt = docopt(__doc__)
 
 def main(opt):
     df = pd.read_csv(opt["<url>"])
